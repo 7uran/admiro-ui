c6d73101c27b5e3d226661e071b468fbf44d8171
7uran <turan.official21@gmail.com>
2024-09-12 17:03:20 +0400

    card create

---
+++ src/components/emptyState/emptyState.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const EmptyState = () => {
+    return (
+        <div className="container mx-auto my-8 space-y-10">
+
+            <div className="max-w-4xl mx-auto px-10 py-4 bg-white rounded-lg shadow-lg">
+                <div className="flex flex-col justify-center py-12 items-center">
+                    <img className="w-64 h-64" src="https://res.cloudinary.com/daqsjyrgg/image/upload/v1690257804/jjqw2hfv0t6karxdeq1s.svg" alt="Empty State" />
+                    <h1 className="text-gray-700 font-medium text-2xl text-center mb-3">Create a Project and get organized!</h1>
+                    <p className="text-gray-500 text-center mb-6">Projects are the backbones of time entry categorization in your workspace.</p>
+                    <div className="flex flex-col justify-center">
+                        <button className="flex items-center px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            Create New Project
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+    );
+};
+
+export default EmptyState;
